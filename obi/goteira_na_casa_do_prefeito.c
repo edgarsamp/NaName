5f3dc0a259bc0427183d342ebb9100342e216270
pinguimdeasadelta <64939397+pinguimdeasadelta@users.noreply.github.com>
2020-09-11 19:15:35 -0300

    Update goteira_na_casa_do_prefeito.c

--- obi/goteira_na_casa_do_prefeito.c
+++ obi/goteira_na_casa_do_prefeito.c
@@ -8,31 +8,30 @@ int main (){
  bool escorreu = true;
 	scanf ("%hd %hd ", &n, &m);
 	char Parede [n][m+1];
-	for (i = 1; i <= n; i++){
+	for (i = 0; i <= n; i++){
 		scanf ("%s", Parede [i]);
 		Parede [i][m] = 0;
 	}
 	while (escorreu){
 		escorreu = false;
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n-1; i++)
 			for (j = 0; j < m; j++)
 				if (Parede [i][j] == 'o'){
-					if (i < n-1){
-						if (Parede [i+1][j] == '.'){
-							Parede [i+1][j] = 'o';
-							escorreu = true;
-						}else 
-							if (Parede [i+1][j] == '#'){
-								if (Parede [i][j+1] == '.'){
-									Parede [i][j+1] = 'o';
-									escorreu = true;
-								}
-								if (Parede [i][j-1] == '.'){
-									Parede [i][j-1] = 'o';
-									escorreu = true;
-								}
+					if (Parede [i+1][j] == '.'){
+						Parede [i+1][j] = 'o';
+						escorreu = true;
+					}else 
+						if (Parede [i+1][j] == '#'){
+							if (Parede [i][j+1] == '.'){
+								Parede [i][j+1] = 'o';
+								escorreu = true;
 							}
-					}
+							if (Parede [i][j-1] == '.'){
+								Parede [i][j-1] = 'o';
+								escorreu = true;
+							}
+						}
+					
 				}
 	}
 	for (i = 0; i < n; i++)
